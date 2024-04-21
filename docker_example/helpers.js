16683f290641862bb2e27201f8d51a530902a0be
Nathan Peek <nmp14fsu@gmail.com>
2024-04-21 00:02:54 -0400

    log db connection

--- docker_example/helpers.js
+++ docker_example/helpers.js
@@ -1,6 +1,7 @@
 const connectToDatabase = () => {
   const dummyPractice = new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log('Database successfully connected.');
       resolve();
     }, 2000);
   });
