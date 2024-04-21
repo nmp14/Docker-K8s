7d210d1e12c41dc4456a3f9ac99556c7aeaef830
Nathan Peek <nmp14fsu@gmail.com>
2024-04-21 02:06:09 -0400

    Make simple docker file example

--- docker_example/index.js
+++ docker_example/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import connectToDatabase from './helpers.js';
 
 const app = express();
-const port = 44631;
+const port = 44361;
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi there!</h2>')
